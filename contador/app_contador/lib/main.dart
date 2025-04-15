97c67bbc4f98a34542b045c2571b463efb864302
kaualayon <limakaua582@gmail.com>
2025-04-15 06:31:17 -0300

    testando a criação do contador

---
+++ contador/app_contador/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contador Simples',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _contador = 0;
+
+  void _incrementar() {
+    setState(() {
+      _contador++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contador'),
+      ),
+      body: Center(
+        child: Text(
+          'Contador: $_contador',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementar,
+        child: Icon(Icons.add),
+        tooltip: 'Incrementar',
+      ),
+    );
+  }
+}
